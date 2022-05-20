6420ef72ffca98d35afb2ab18e77c7833152f772
ekureal <12ajyfwnbox17c27@gmail.com>
2022-05-20 17:04:19 +0900

    Update menu.py
    コードの見やすさ、利用のしやすさを改善
    今後貸出機能の追加、保守性の高いコードに改修予定

--- menu.py
+++ menu.py
@@ -31,41 +31,40 @@ def menu():
 
         # 1,2はデータベースから情報を取得し、表示する処理
         elif keyPress == '1':
-            stay = statusClass("select 学番,学年,名前,最終更新 from 学生情報")
-            stay.status()
+            stay = statusClass()
+            stay.SQLstatus("select 学籍番号,学年,氏名,最終更新 from 学生情報")
             # 取り出したデータに対して列を設定して表形式で出力する
-            stay.printdf('学籍番号', '学年', '氏名', '最終更新')
+            print(stay.printdf('学籍番号', '学年', '氏名', '最終更新'))
         elif keyPress == '2':
-            tool = statusClass("select 備品名,備品状態 from 備品情報")
-            tool.status()
-            tool.printdf('備品名', '備品状態')
+            tool = statusClass()
+            tool.SQLstatus("select 資産番号,備品名,備品状態,最終更新 from 備品情報")
+            print(tool.printdf('資産番号','備品名', '備品状態','最終更新'))
 
         # 3:学生情報登録処理
         elif keyPress == '3':
-            student = resisterStudentClass()
-            # アクセスするuser,password,host,DB,charsetの設定をする
-            student.configSQL('user', 'passwd', 'host', 'db', 'utf8')
+            student = resisterStudentClass(0x300B, 4)
             try:
                 # ---------------------nfc接続,データ取得処理----------------------
                 student.nfcConnect()
+                idm = student.getidm()
                 # --------------------------------------------------------------
 
                 # -------------------idmの照合処理(テーブルを指定)------------------
-                student.idmSQL('学生情報')
-                # データの重複登録が行われないよう、データベース内に同じidmがあった場合にエラー出力
-                student.idmCheckerT('既に登録されている学生情報です、メニューに戻ります')
+                SQLidm = student.SQLRead("select idm from 学生情報 where idm = '{}'".format(idm))
+                # データの重複登録が行われないよう、データベース内に同じidmがあった場合にエラー出力,19に治す
+                student.isResister(idm, str(SQLidm)[3:19], '既に登録されている学生情報です、メニューに戻ります')
                 # --------------------------------------------------------------
 
                 # -------------idm,取得した学生情報の呼び出し,加工処理----------------
-                idm = student.getidm()
                 editList = student.geteditList()
+                student.editListInput('氏名：')
                 # 学生情報の加工処理
                 student.editdict(editList, '学籍番号', '学年', '氏名')
                 # --------------------------------------------------------------
 
                 # --------------------学生情報の登録処理---------------------------
-                res = student.resDecoretor(student.insertSQL)
-                res('学生情報', 'idm,学番,学年,名前', idm, *editList)
+                res = student.resDecoretor(student.SQLCommit)
+                res("INSERT INTO 学生情報(idm, 学籍番号, 学年, 氏名) VALUES('{}',{})".format(idm,str(editList)[1:-1]))
                 # --------------------------------------------------------------
             # OS,ValueErrorはnfcリーダーが接続されていない,無効な入力の際に利用
             except OSError:
@@ -80,25 +79,26 @@ def menu():
         # 4:備品登録処理,備品名の入力処理以外は3と同様
         elif keyPress == '4':
             tool = resisterItemClass()
-            tool.configSQL('user', 'passwd', 'host', 'db', 'utf8')
 
             try:
                 tool.nfcConnect()
+                idm = tool.getidm()
 
-                tool.idmSQL('備品情報')
-                tool.idmCheckerT('既に登録されている備品IDです、メニューに戻ります')
+                SQLidm = tool.SQLRead("select idm from 備品情報 where idm = '{}'".format(idm))
+                tool.isResister(idm, str(SQLidm)[3:19], '既に登録されている備品情報です、メニューに戻ります')
 
                 # ---------------備品名の入力処理,任意の備品名の入力------------------
+                print('資産番号を決定してください')
+                tool.editListInput('資産番号:')
                 print('備品名を決定してください')
-                tool.editListInput()
+                tool.editListInput('備品名：')
                 # --------------------------------------------------------------
 
-                idm = tool.getidm()
                 editList = tool.geteditList()
-                tool.editdict(editList, '備品名')
+                tool.editdict(editList,'資産番号','備品名')
 
-                res = tool.resDecoretor(tool.insertSQL)
-                res('備品情報', 'idm,備品名', idm, *editList)
+                res = tool.resDecoretor(tool.SQLCommit)
+                res("INSERT INTO 備品情報(idm,資産番号,備品名) VALUES('{}',{})".format(idm,str(editList)[1:-1]))
 
             except OSError:
                 print('OSError：nfcリーダーが接続されていません。メニューに戻ります')
@@ -110,26 +110,25 @@ def menu():
 
         # 5:学年変更処理,学年変更部分以外はほぼ3と同様
         elif keyPress == '5':
-            hen = changeSchoolYearClass()
-            hen.configSQL('user', 'passwd', 'host', 'db', 'utf8')
+            change = changeSchoolYearClass()
             try:
-                hen.nfcConnect()
+                change.nfcConnect()
+                idm = change.getidm()
 
-                hen.idmSQL('学生情報')
-                # 登録されていないデータに対してエラーの出力
-                hen.idmCheckerF('登録されていない学生情報です、メニューに戻ります')
+                SQLidm = change.SQLRead("select idm from 学生情報 where idm = '{}'".format(idm))
+                change.isNotResister(idm, str(SQLidm)[3:19], '登録されていない学生です')
 
                 # ---------------学年取得処理(上:変更前,下:変更後)-------------------
-                hen.schoolYearSQL()
-                hen.inputYear()
+                SQLYear = change.SQLRead("select 学年 from 学生情報 where idm='{}'".format(idm))
+                editList = change.geteditList()
+                editList.append(str(SQLYear[1])[1:2])
+                change.inputYear()
                 # --------------------------------------------------------------
+                change.editdict(editList, '変更前の学年', '変更後の学年')
 
-                idm = hen.getidm()
-                editList = hen.geteditList()
-                hen.editdict(editList, '変更前の学年', '変更後の学年')
 
-                res = hen.resDecoretor(hen.updateSQL)
-                res('学生情報', '学年', editList[1])
+                res = change.resDecoretor(change.SQLCommit)
+                res("UPDATE 学生情報 SET 学年 = {} where idm = '{}'".format(int(editList[1]),idm))
 
             except OSError:
                 print('OSError：nfcリーダーが接続されていません。メニューに戻ります')
@@ -137,7 +136,7 @@ def menu():
                 print(v)
             except Exception as e:
                 print('Errorが発生しました:%s' % (e))
-                hen.errorLog()
+                change.errorLog()
 
         # develop:プログラム改善用
         elif keyPress == 'develop':
